0bf2a9c20001aa4b104479326ea23882eaa87cf3
Carl Harris <elgoogemail2007@gmail.com>
2021-01-24 02:05:51 -0500

    init

---
+++ daterange.py
@@ -0,0 +1,57 @@
+import argparse
+import sys
+
+from datetime import date
+from datetime import timedelta
+
+def daterange(*args):
+    """
+    daterange(stop) -> date generator
+    daterange(start, stop[, step]) -> date generator
+
+    Like `range` for dates. When start is omitted it defaults to today.
+    """
+    if len(args) == 1:
+        stop = args[0]
+        start = date.today()
+        step = 1
+    elif len(args) == 2:
+        start, stop = args
+        step = 1
+    elif len(args) == 3:
+        start, stop, step = args
+    step = timedelta(days=step)
+    while start < stop:
+        yield start
+        start += step
+
+def main(argv=None):
+    "Generate a range of dates."
+    parser = argparse.ArgumentParser(
+        description=main.__doc__, prog='daterange', epilog=daterange.__doc__)
+    parser.add_argument(
+        'rangeargs', nargs='+', help="stop. start, stop[, step].")
+    parser.add_argument(
+        '-z', '--null', action='store_true',
+        help="separate by null instead of newline")
+    parser.add_argument(
+        '-o', '--output', default=sys.stdout, help='output file')
+    parser.add_argument(
+        '-f', '--format', default='%Y-%m-%d', help='output date format')
+    args = parser.parse_args(argv)
+
+    if len(args.rangeargs) > 3:
+        parser.error('one to three arguments required')
+
+    rangeargs = [date.fromisoformat(arg) for arg in args.rangeargs[:2]]
+    if len(args.rangeargs) == 3:
+        rangeargs += [int(args.rangeargs[2])]
+
+    sep = '\0' if args.null else '\n'
+
+    for d in daterange(*rangeargs):
+        print(d.strftime(args.format), end=sep, file=args.output)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='daterange',
+      version='0.1',
+      description='Like range for dates',
+      license='MIT',
+      py_modules=['daterange'],
+  )
